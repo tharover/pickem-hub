848ea7df419c8db2de4fb59249aa3712a4b233f7
tharover <tharover@gmail.com>
2025-07-05 10:05:30 -0400

    figured out pathing for data files

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -4,7 +4,7 @@ export default function Leaderboard() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/data/leaderboard.json')
+    fetch('/pickem-hub/data/leaderboard.json')
       .then(res => res.json())
       .then(json => {
         // Optional: group and summarize by player
